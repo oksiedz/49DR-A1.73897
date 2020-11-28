aed578e9811a3082174313e2dae695bc1c2235f4
Tomasz Jan Oksiędzki <oksiedz@gmail.com>
2020-11-28 19:09:43 +0100

    Imported pictures of rock, paper, scissors and question mark to the Form for the first ObjectBox

--- RockPaperScissors/RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -49,6 +49,7 @@ namespace RockPaperScissors
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::RockPaperScissors.Properties.Resources.scissiors;
             this.pictureBox1.Location = new System.Drawing.Point(231, 44);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(160, 175);
